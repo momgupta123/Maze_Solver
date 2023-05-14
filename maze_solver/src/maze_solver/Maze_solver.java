92a60fae6a818f1d117f1a19794734cc023e070f
Raushan kumar <98281059+momgupta123@users.noreply.github.com>
2023-05-15 02:35:01 +0530

    Add files via upload

---
+++ maze_solver/src/maze_solver/Maze_solver.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maze_solver;
+
+/**
+ *
+ * @author Roshan
+ */
+import javax.swing.JFrame;
+public class Maze_solver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        private int[][] maze=
+        {{1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,0,1,0,1,0,0,0,0,0,1},
+            {1,0,1,0,0,0,1,0,1,1,1,0,1},
+            {1,0,1,1,1,1,1,0,0,0,0,0,1},
+            {1,0,0,1,0,0,0,0,1,1,1,0,1},
+            {1,0,1,0,1,1,1,0,1,0,0,0,1},
+            {1,0,1,0,1,0,0,0,1,1,1,0,1},
+            {1,0,1,0,1,1,1,0,1,0,1,0,1},
+            {1,0,0,0,0,0,0,0,0,0,1,9,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1}            
+        };
+        
+        public List<Integer> path=new ArrayList<>();
+        
+        
+    }
+    
+}
